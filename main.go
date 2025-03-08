360e373d23486be48ab928c86627106ea1d98319
ilaumjd <ilaumjd@gmail.com>
2025-03-08 15:01:39 +0700

    add logging middleware

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/ilaumjd/pendecc/database"
 	"github.com/ilaumjd/pendecc/handlers"
-	"github.com/rs/cors"
 	_ "github.com/lib/pq"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -27,16 +30,48 @@ func main() {
 	mux.HandleFunc("POST /urls", urlHandler.CreateShortUrl)
 
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"*"},  // Be more specific in production
-		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders: []string{"*"},
+		AllowedOrigins:   []string{"*"}, // Be more specific in production
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
 		AllowCredentials: true,
 	})
-	handler := c.Handler(mux)
+	handler := c.Handler(loggingMiddleware(mux))
 
 	server := &http.Server{
 		Addr:    ":5100",
 		Handler: handler,
 	}
 	log.Fatal(server.ListenAndServe())
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Log the request
+		logRequest(r)
+
+		// Call the next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
+func logRequest(r *http.Request) {
+	logEntry := fmt.Sprintf("[%s] %s %s %s\n",
+		time.Now().Format("2006-01-02 15:04:05"),
+		r.RemoteAddr,
+		r.Method,
+		r.URL.Path,
+	)
+
+	// Open the log file in append mode, create it if it doesn't exist
+	f, err := os.OpenFile(os.ExpandEnv("$HOME/api_log.txt"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Printf("Error opening log file: %v", err)
+		return
+	}
+	defer f.Close()
+
+	// Write the log entry to the file
+	if _, err := f.WriteString(logEntry); err != nil {
+		log.Printf("Error writing to log file: %v", err)
+	}
 }
